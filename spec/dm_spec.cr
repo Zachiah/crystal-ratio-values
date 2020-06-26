52b2b7fc4840727932ef5d51f76aa0d67b4dd764
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2020-06-26 09:23:44 -0600

    Test Drive Dm

---
+++ spec/dm_spec.cr
@@ -0,0 +1,19 @@
+require "./spec_helper"
+
+describe Dm do
+    it "converts dm to in" do
+        Dm.new(0.254).to_in.value.should eq(In.new(1).value)
+    end
+
+    it "converts dm to cm" do
+        Dm.new(10).to_in.value.should eq(Cm.new(100).value)
+    end
+
+    it "converts dm to mm" do
+        Dm.new(10).to_mm.value.should eq(Mm.new(1000).value)
+    end
+
+    it "converts dm to ft" do
+        Dm.new(0.254).to_in.value.should eq(Ft.new(1/12).value)
+    end
+end
